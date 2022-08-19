b18f8a0e3f1bc95185ebde85adc6cb3b7830ffb3
Carlos Herrera Vázquez <84268470+Lentejas1@users.noreply.github.com>
2022-08-19 13:45:00 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from bs4 import BeautifulSoup
+import urllib.request
+URL = "https://rk9.gg/pairings/worlds-2022-vgc#P2"
+html = urllib.request.urlopen(URL).read()
+soup = BeautifulSoup(html, "html.parser")
+
+results = soup.get_text()
+lista = results.split()
+print(lista)
+indexes = []
+country = "[ES]"
+
+for i in range(len(lista)):
+    if lista[i] == country:
+        indexes.append(i)
+
+data = []
+jugadores = []
+partidas = []
+
+data = list(set(data))
+
+st.title("Españoles en Pokémon WCS'22")
+
+datos_buenos = []
+records = []
+for num in indexes:
+    records.append(lista[num + 1])
+    jugadores.append(lista[num - 2] + " " + lista[num - 1])
+
+jugadores_unique = list(set(jugadores))
+print(jugadores_unique)
+print(datos_buenos)
+for jugador in jugadores_unique:
+    lista_ind = []
+    for i in range(len(jugadores)):
+        if jugadores[i] == jugador:
+            lista_ind.append(i)
+    for j in lista_ind:
+        indice = max(lista_ind)
+    st.markdown(jugador + " " + records[indice])
