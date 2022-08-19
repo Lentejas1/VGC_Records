5a2af986c5fa2e954d9318eeb90e657f98c10bb1
Carlos Herrera Vázquez <84268470+Lentejas1@users.noreply.github.com>
2022-08-19 14:06:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ partidas = []
 data = list(set(data))
 
 st.title("Españoles en Pokémon WCS'22")
-st.markdown(r"# por Carlos Herrera (@miniherrera11)")
+st.markdown(r"### por Carlos Herrera [(@miniherrera11)](https://twitter.com/miniherrera11)")
 
 datos_buenos = []
 records = []
