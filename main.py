c4bc4b13d23388ff12a3bac4ff7c48013bcd9954
Carlos Herrera Vázquez <84268470+Lentejas1@users.noreply.github.com>
2022-08-20 10:45:49 +0200

    Updated

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-#import requests
 import streamlit as st
 from bs4 import BeautifulSoup
 import urllib.request
+
+st.title("Pokémon VGC records by country")
+st.markdown("Data from [RK9 Labs](https://rk9.gg/). Developed with :heart: by [@miniherrera11](https://twitter.com/miniherrera11).")
+
 URL = "https://rk9.gg/pairings/worlds-2022-open-vgc"
+st.markdown(f"**Tournament displayed: {URL}**")
+checkbox = st.checkbox("To choose another one, please enable this next checkbox and paste the pairings' link from RK9 Labs below.")
+
+if checkbox:
+    url = st.text_input("Paste it here")
+    if len(url) == 0:
+        pass
+    else:
+        URL = url
+
+country = st.text_input("Write the tag (e.g.: '[ES]')")
+
 html = urllib.request.urlopen(URL).read()
 soup = BeautifulSoup(html, "html.parser")
 
@@ -10,20 +25,11 @@ results = soup.get_text()
 lista = results.split()
 indexes = []
 
-st.title("Pokémon WCS'22 records by country")
-st.markdown("Data from [RK9 Labs](https://rk9.gg/pairings/worlds-2022-vgc#P2). Developed with :heart: by [@miniherrera11](https://twitter.com/miniherrera11).")
-country = st.text_input("Write the tag (e.g.: [ES])")
-
 for i in range(len(lista)):
     if lista[i] == country:
         indexes.append(i)
 
-data = []
 jugadores = []
-partidas = []
-
-data = list(set(data))
-
 
 datos_buenos = []
 records = []
