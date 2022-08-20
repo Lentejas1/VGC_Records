c5555e2b93782bc27b8c5987696b5cf862bc9454
Carlos Herrera Vázquez <84268470+Lentejas1@users.noreply.github.com>
2022-08-20 13:06:23 +0200

    Commented

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from bs4 import BeautifulSoup
 import urllib.request
 
 st.title("Pokémon VGC records by country")
-st.markdown("Data from [RK9 Labs](https://rk9.gg/). Developed with :heart: by [@miniherrera11](https://twitter.com/miniherrera11).")
+st.markdown(
+    "Data from [RK9 Labs](https://rk9.gg/). Developed with :heart: by [@miniherrera11](https://twitter.com/miniherrera11).")
 
 URL = "https://rk9.gg/pairings/worlds-2022-open-vgc"
 st.markdown(f"**Default tournament displayed: {URL}**")
-checkbox = st.checkbox("To choose another one, please enable this checkbox and paste the pairings' link from RK9 Labs below.")
+checkbox = st.checkbox(
+    "To choose another one, please enable this checkbox and paste the pairings' link from RK9 Labs below.")
 
 if checkbox:
     url = st.text_input("Paste it here")
@@ -21,29 +23,27 @@ country = st.text_input("Write the tag (e.g.: '[ES]')")
 html = urllib.request.urlopen(URL).read()
 soup = BeautifulSoup(html, "html.parser")
 
-results = soup.get_text()
-lista = results.split()
-indexes = []
+results = soup.get_text()  # Gets the text from the html
+words = results.split()  # Splits results into words
+indices = []  # Stores the position inside the 'list' array of all tags from the same country
 
-for i in range(len(lista)):
-    if lista[i] == country:
-        indexes.append(i)
+for i in range(len(words)):  # Checks if words are the tag we wanted
+    if words[i] == country:
+        indices.append(i)
 
-jugadores = []
+players = []  # Stores all players from wished country
+records = []  # Stores all records from wished country with the same order
 
-datos_buenos = []
-records = []
-for num in indexes:
-    records.append(lista[num + 1])
-    jugadores.append(lista[num - 2] + " " + lista[num - 1])
+for num in indices:  # Appends players and their records from indices
+    records.append(words[num + 1])
+    players.append(words[num - 2] + " " + words[num - 1])
 
-jugadores_unique = list(set(jugadores))
+players_unique = list(set(players))  # Eliminates duplicated names
 
-for jugador in jugadores_unique:
-    lista_ind = []
-    for i in range(len(jugadores)):
-        if jugadores[i] == jugador:
-            lista_ind.append(i)
-    for j in lista_ind:
-        indice = max(lista_ind)
-    st.markdown(jugador + " " + records[indice])
+for player in players_unique:  # Checks from all entries of a players, which is the most recent
+    list_ind = []
+    for i in range(len(players)):
+        if players[i] == player:
+            list_ind.append(i)
+    index = max(list_ind)
+    st.markdown(player + " " + records[index])
